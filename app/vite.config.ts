07eae63816d6d42c4936032b29bece6e22f8feec
Simranjeet Singh <simranjeet@s25.digital>
2022-04-18 12:16:11 +0530

    restructing code

---
+++ app/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    // "process.env.SHOPIFY_API_KEY": JSON.stringify(process.env.SHOPIFY_API_KEY),
+    // appOrigin: JSON.stringify(process.env.SHOPIFY_APP_URL.replace(/https:\/\//, "")),
+  },
+  plugins: [react()],
+});
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import * as fs from "fs";
 import Express from "express";
 import compression from "compression";
 import serveStatic from "serve-static";
