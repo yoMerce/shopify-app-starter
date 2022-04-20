0fe5b40526ede1a7097987a9450708e716c27d79
Simranjeet Singh <simranjeet@s25.digital>
2022-04-20 14:50:43 +0530

    [Adding middlewrares from the shopify code sample]

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import path from "path";
+import * as fs from "fs";
 import Express from "express";
 import compression from "compression";
 import serveStatic from "serve-static";
 import cookieParser from "cookie-parser";
 import { ApiVersion, Shopify } from "@shopify/shopify-api";
 import SessionStorage from "./sessions";
+import applyAuthMiddleware from "./auth";
+import { verifyRequest } from "./middlewares";
 
-// add session storage
+const ACTIVE_SHOPIFY_SHOPS = {};
 
 // connect mongodb
 
@@ -18,7 +21,7 @@ Shopify.Context.initialize({
   HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
   API_VERSION: ApiVersion.April22,
   IS_EMBEDDED_APP: true,
-  SESSION_STORAGE: SessionStorage,
+  // SESSION_STORAGE: SessionStorage,
 });
 
 // register webhooks
@@ -42,14 +45,13 @@ Shopify.Context.initialize({
 // });
 
 const app = Express();
-// app.set("top-level-oauth-cookie", "shopify_top_level_oauth");
+app.set("top-level-oauth-cookie", "shopify_top_level_oauth");
 app.set("use-online-tokens", true);
+app.set("active-shopify-shops", ACTIVE_SHOPIFY_SHOPS);
 
 app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
 
-// add auth middleware
-
-//app.use("/webhooks", webhookRoutes); //webhookRotues
+applyAuthMiddleware(app);
 
 // Graphql proxy for Shopify
 // app.post("/graphql", verifyRequest(app), async (req, res) => {
@@ -61,9 +63,34 @@ app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
 //   }
 // });
 app.use(Express.json());
+app.use((req, res, next) => {
+  const shop = req.query.shop;
+  if (Shopify.Context.IS_EMBEDDED_APP && shop) {
+    res.setHeader(
+      "Content-Security-Policy",
+      `frame-ancestors https://${shop} https://admin.shopify.com;`
+    );
+  } else {
+    res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
+  }
+  next();
+});
+
 app.use(compression());
 app.use(serveStatic(path.resolve(__dirname, "../client"), { index: ["index.html"] }));
 
-// app.use("/", userRoutes);
+app.use("/*", (req, res, next) => {
+  const shop = req.query.shop;
+  console.log(shop);
+
+  // Detect whether we need to reinstall the app, any request from Shopify will
+  // include a shop in the query parameters.
+  // if (app.get("active-shopify-shops")[shop] === undefined && shop) {
+  //   res.redirect(`/auth?shop=${shop}`);
+  // } else {
+  //   next();
+  // }
+  next();
+});
 
 export default app;
