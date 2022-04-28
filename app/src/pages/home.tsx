1a2e7fdf8ee2d03f303bcd623e4b082541625cf6
Simranjeet Singh <simranjeet@s25.digital>
2022-04-28 11:27:02 +0530

    [Adding an empty state]

--- app/src/pages/home.tsx
+++ app/src/pages/home.tsx
@@ -1,5 +1,18 @@
+import { Card, EmptyState } from "@shopify/polaris";
+
 function Home() {
-  return <div>This is home page</div>;
+  return (
+    <Card sectioned>
+      <EmptyState
+        heading="Manage your App"
+        action={{ content: "Create an App" }}
+        secondaryAction={{ content: "Learn more", url: "https://yomerce.com" }}
+        image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+      >
+        <p>This is a shopify-starter-app by yomerce</p>
+      </EmptyState>
+    </Card>
+  );
 }
 
 export default Home;
