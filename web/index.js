28cdfcb1268642a110e4c18cdaff7a80d2e27e00
Simranjeet Singh <simranjeet@s25.digital>
2022-06-28 15:08:17 +0530

    [setup server and startup files]

--- web/index.js
+++ web/index.js
@@ -1,179 +1,18 @@
-// @ts-check
-import { join } from "path";
-import express from "express";
-import cookieParser from "cookie-parser";
-import { Shopify, ApiVersion } from "@shopify/shopify-api";
+//@ts-check
 
-import applyAuthMiddleware from "./middleware/auth.js";
-import verifyRequest from "./middleware/verify-request.js";
-import { setupGDPRWebHooks } from "./gdpr.js";
-import { BillingInterval } from "./helpers/ensure-billing.js";
-
+import { createServer, logger } from "./server.js";
 import Config from "./config.js";
+import { getDbDriver } from "./db";
+import { setupHandlers } from "./webhooks";
 
-const USE_ONLINE_TOKENS = true;
-const TOP_LEVEL_OAUTH_COOKIE = "shopify_top_level_oauth";
+async function start() {
+  const db = await getDbDriver();
 
-const isTest = Config.ISTEST;
+  setupHandlers(db, logger);
 
-// const versionFilePath = "./version.txt";
-// let templateVersion = "unknown";
-// if (fs.existsSync(versionFilePath)) {
-//   templateVersion = fs.readFileSync(versionFilePath, "utf8").trim();
-// }
-
-Shopify.Context.initialize({
-  API_KEY: Config.SHOPIFY.key,
-  API_SECRET_KEY: Config.SHOPIFY.secret,
-  SCOPES: Config.SHOPIFY.scopes.split(","),
-  HOST_NAME: Config.SHOPIFY.host.replace(/https?:\/\//, ""),
-  HOST_SCHEME: Config.SHOPIFY.host.split("://")[0],
-  API_VERSION: ApiVersion.April22,
-  IS_EMBEDDED_APP: true,
-  SESSION_STORAGE: Shopify.Session.MongoDBSessionStorage.withCredentials(
-    Config.DB.host,
-    Config.DB.name,
-    Config.DB.user,
-    Config.DB.password,
-    { sessionCollectionName: "sessions" }
-  ),
-  USER_AGENT_PREFIX: `App Starter`,
-});
-
-// Storing the currently active shops in memory will force them to re-login when your server restarts. You should
-// persist this object in your app.
-const ACTIVE_SHOPIFY_SHOPS = {};
-Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
-  path: "/api/webhooks",
-  webhookHandler: async (topic, shop, body) =>
-    delete ACTIVE_SHOPIFY_SHOPS[shop],
-});
-
-// The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
-// See the ensureBilling helper to learn more about billing in this template.
-const BILLING_SETTINGS = {
-  required: false,
-  // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
-  // chargeName: "My Shopify One-Time Charge",
-  // amount: 5.0,
-  // currencyCode: "USD",
-  // interval: BillingInterval.OneTime,
-};
-
-// This sets up the mandatory GDPR webhooks. You’ll need to fill in the endpoint
-// in the “GDPR mandatory webhooks” section in the “App setup” tab, and customize
-// the code when you store customer data.
-//
-// More details can be found on shopify.dev:
-// https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks
-setupGDPRWebHooks("/api/webhooks");
-
-// export for test use only
-export async function createServer(
-  root = process.cwd(),
-  isProd = process.env.NODE_ENV === "production",
-  billingSettings = BILLING_SETTINGS
-) {
-  const app = express();
-  app.set("top-level-oauth-cookie", TOP_LEVEL_OAUTH_COOKIE);
-  app.set("active-shopify-shops", ACTIVE_SHOPIFY_SHOPS);
-  app.set("use-online-tokens", USE_ONLINE_TOKENS);
-
-  app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
-
-  applyAuthMiddleware(app, {
-    billing: billingSettings,
-  });
-
-  app.post("/api/webhooks", async (req, res) => {
-    try {
-      await Shopify.Webhooks.Registry.process(req, res);
-      console.log(`Webhook processed, returned status code 200`);
-    } catch (error) {
-      console.log(`Failed to process webhook: ${error}`);
-      if (!res.headersSent) {
-        res.status(500).send(error.message);
-      }
-    }
-  });
-
-  // All endpoints after this point will require an active session
-  app.use(
-    "/api/*",
-    verifyRequest(app, {
-      billing: billingSettings,
-    })
-  );
-
-  app.get("/api/products-count", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(req, res, true);
-    const { Product } = await import(
-      `@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
-    );
-
-    const countData = await Product.count({ session });
-    res.status(200).send(countData);
-  });
-
-  app.post("/api/graphql", async (req, res) => {
-    try {
-      const response = await Shopify.Utils.graphqlProxy(req, res);
-      res.status(200).send(response.body);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  });
-
-  app.use(express.json());
-
-  app.use((req, res, next) => {
-    const shop = req.query.shop;
-    if (Shopify.Context.IS_EMBEDDED_APP && shop) {
-      res.setHeader(
-        "Content-Security-Policy",
-        `frame-ancestors https://${shop} https://admin.shopify.com;`
-      );
-    } else {
-      res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
-    }
-    next();
-  });
-
-  if (isProd) {
-    const compression = await import("compression").then(
-      ({ default: fn }) => fn
-    );
-    const serveStatic = await import("serve-static").then(
-      ({ default: fn }) => fn
-    );
-    app.use(compression());
-    app.use(serveStatic(Config.PROD_INDEX_PATH));
-  }
-
-  app.use("/*", async (req, res, next) => {
-    const shop = req.query.shop;
-
-    // Detect whether we need to reinstall the app, any request from Shopify will
-    // include a shop in the query parameters.
-    if (app.get("active-shopify-shops")[shop] === undefined && shop) {
-      res.redirect(`/api/auth?shop=${shop}`);
-    } else {
-      // res.set('X-Shopify-App-Nothing-To-See-Here', '1');
-      const fs = await import("fs");
-      const fallbackFile = join(
-        isProd ? Config.PROD_INDEX_PATH : Config.DEV_INDEX_PATH,
-        "index.html"
-      );
-      res
-        .status(200)
-        .set("Content-Type", "text/html")
-        .send(fs.readFileSync(fallbackFile));
-    }
-  });
-
-  return { app };
+  return createServer().then(({ app }) => app.listen(Config.API.port));
 }
 
-if (!isTest) {
-  createServer().then(({ app }) => app.listen(Config.API.port));
+if (!Config.ISTEST) {
+  start();
 }
